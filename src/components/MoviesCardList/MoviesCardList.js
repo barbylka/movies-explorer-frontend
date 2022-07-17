2b828c3aeb72940da98b7135bdd546b55a28cce3
Irina Stafeeva <irina.staffeewa@gmail.com>
2022-07-17 18:25:39 +0200

    fix: number of cards to add

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -24,7 +24,6 @@ function MoviesCardList({ cards, moviesErrorMessage, isSaved, onSaveMovie, onDel
   const [cardNumber, setCardNumber] = React.useState(numberOfCards);
 
   const showMoreCards = () => {
-    console.log(addCardsNumber());
     setCardNumber(cardNumber + addCardsNumber());
   }
 
